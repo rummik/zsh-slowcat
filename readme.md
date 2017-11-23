8f004b8ededf275db2baf21e7b17abe1da546e26
rummik <k@9k1.us>
2017-11-23 05:21:10 -0500

    Bugfixes, add help and version args

--- readme.md
+++ readme.md
@@ -4,3 +4,9 @@ For those times when `cat` is just way too fast.
 Installable with:
 - [Zplug](https://github.com/zplug/zplug): `zplug "rummik/zsh-slowcat"`
 - [Antigen](https://github.com/zsh-users/antigen): `antigen bundle rummik/zsh-slowcat`
+
+Usage:
+- Hello World: `slowcat <<< 'Hello World!'`
+- Change the delay with the `SLOWCAT_DELAY` env var: `SLOWCAT_DELAY=0.5 slowcat <<< 'Hello World!'`
+
+
--- slowcat.plugin.zsh
+++ slowcat.plugin.zsh
@@ -2,13 +2,32 @@
 : ${SLOWCAT_DELAY=0.05}
 
 function slowcat {
-	local text i
-	text=$(cat $@)
+	if [[ x$1 == 'x--version' ]]; then
+		slowcat <<-VERSION
+		slowcat 1.0
+		Copyright (c) 2017 Kimberly Zick (rummik)
+		Licensed under the MIT license
 
-	for ((i=0; i<=$#text; i++)); do
-		print -n $text[i]
+		CAT VERSION OUTPUT:
+		VERSION
+	elif [[ x$1 == 'x--help' ]]; then
+		slowcat <<-USAGE
+		Usage: slowcat [OPTION]... [FILE]...
+		Slowly concatenate FILE(s) to standard output.
+
+		$(cat --help | head -n -8 | tail -n +4)
+
+		Examples:
+		  slowcat f - g Slowly output f's contents, then standard input, then g's contents.
+		  slowcat       Slowly copy standard input to standard output.
+		USAGE
+
+		return
+	fi
+
+	local char
+	cat $@ | while read -k1 -u0 char; do
+		print -n -- $char
 		sleep $SLOWCAT_DELAY
 	done
-
-	print
 }
